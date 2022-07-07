22a38f8d8cb21de7324b710f94e9eda54dd9d74a
philipp <spiderpic98@gmail.com>
2022-07-07 18:45:28 +0200

    Server/Client

--- src/main/java/Server/Server.java
+++ src/main/java/Server/Server.java
@@ -9,15 +9,18 @@ import java.net.*;
 
 public class Server extends Thread
 {
-   public static ServerSocket server;
 
+    public static ServerSocket server;
 
-    public static Socket client;
     static int port;
+    public static Socket client;
+
+
     public Server(int port) throws IOException
     {
         this.port = port;
-
+        client = new Socket("localhost",port);
+        server = new ServerSocket(port);
 
 
         System.out.println(port);
@@ -33,15 +36,14 @@ public class Server extends Thread
 
             try
             {
-                if(!client.isConnected()){
-                    System.out.println("waiting for client");
-                }else{
-                    System.out.println("connected");
-                }
-
                 ConnectionListener connectionListener;
                 connectionListener = new ConnectionListener();
                 connectionListener.start();
+                if(!client.isConnected()){
+                    System.out.println("waiting for client");
+                }
+
+
 
 
 
@@ -53,9 +55,5 @@ public class Server extends Thread
 
     }
 
-    public static void main(String[] args) throws IOException {
-     server = new ServerSocket(Server.port);
-     Server server= new Server(Server.port);
-     server.start();
-    }
+
 }
