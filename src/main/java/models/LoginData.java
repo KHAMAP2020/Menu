faba9a1d311d4734afe80c5011306f263fed5fc8
Allison Hoffmann <ahoffmann@stud.hs-bremen.de>
2022-07-10 21:30:33 +0200

    LoginData Kommentiert

--- src/main/java/models/LoginData.java
+++ src/main/java/models/LoginData.java
@@ -1,6 +1,8 @@
 package models;
 
 /**
+ * Login Daten die der Registerdialog zurück geben soll
+ *
  * @author A.Hoffmann 5137817
  */
 public class LoginData
@@ -8,39 +10,67 @@ public class LoginData
 //-------------------------------------------------------------
 //Datenfeld
   
+  /**
+   * Nutzername, der Später im Chat für
+   * alle sichtbar sein soll um zu sehen, wer die Nachricht
+   * geschrieben hat.
+   */
   public static String name = null;
+  
+  /**
+   * Portnummer des Servers
+   */
   public static int port = 0;
+  
+  /**
+   * Adresse des Hosts
+   */
   public static String HostAdress = null;
- 
+
 //-------------------------------------------------------------
 //Methoden
   
-  public LoginData(String name, String HostAdress, int port)
+  /**
+   * Konstruktor der Login-Daten
+   *
+   * @param name Name des Anwenders
+   * @param HostAddress Adresse des Hosts
+   * @param port Portnummer
+   */
+  public LoginData(String name, String HostAddress, int port)
   {
-    LoginData.name = name;
-    LoginData.HostAdress = HostAdress;
-    LoginData.port = port;
+    this.name = name;
+    this.HostAdress = HostAddress;
+    this.port = port;
   }
   
   //-----------------------------------------------------------
   //Getter
   
+  /**
+   * Gibt den Namen des Anwenders zurück
+   * @return den Namen des Anwenders
+   */
   public String getName()
   {
-    return name;
+    return this.name;
   }
   
+  /**
+   * Gibt die Adresse des Hosts zurück
+   * @return die Adresse des Hosts
+   */
   public String getHostAdress()
   {
-    return HostAdress;
+    return this.HostAdress;
   }
   
+  /**
+   * gibt die Portnummer zurück
+   * @return die Portnummer
+   */
   public int getPort()
   {
-    return port;
+    return this.port;
   }
-  
-  //-----------------------------------------------------------
-  //Setter
-
 }
